4002c6c04c14ff05209cfb3814dbc3fca404c0f9
Necroliner <noe.boyere@gmail.com>
2023-04-28 16:21:15 +0200

    v1 commit

--- functions/fetchScores.js
+++ functions/fetchScores.js
@@ -1,28 +1,40 @@
-import fetch from 'node-fetch';
-import jsdom from 'jsdom';
+const request = require('request');
+const cheerio = require('cheerio');
 
-const { JSDOM } = jsdom;
+exports.handler = async function(event, context, callback) {
 
-export async function handler(event, context) {
-  const username = event.queryStringParameters.username;
-  const targetUrl = `https://www.jetpunk.com/users/${username}`;
+  const userName = 'Tuperwak';
+  const domain = 'www.jetpunk.com';
+  const urlString = `${domain}/users/${userName}`;
 
-  const response = await fetch(targetUrl);
-  const data = await response.text();
-
-  const { document } = new JSDOM(data).window;
-  const scoreElement = document.querySelector('.shield-total .level-points');
-
-  if (scoreElement) {
-    const score = parseInt(scoreElement.textContent.replace(/,/g, ''), 10);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ score }),
-    };
-  }
-
-  return {
-    statusCode: 404,
-    body: JSON.stringify({ error: 'User not found' }),
+  const options = {
+    url: `https://cors-anywhere.herokuapp.com/${urlString}`,
+    headers: { 'User-Agent': 'netlify_scraper' },
+    followRedirect: false, // Disable redirection because we only care about the initial GET response
+    encoding: null, // Don't set an explicit encoding, letting Node handle auto-detecting text encoding
   };
+  const htmlContent = await requestGetWithOptions(options);
+
+  const $ = cheerio.load(htmlContent);
+  const pointsValue = $('div[class*="level-points"]').text();
+
+  exports.callbackLogger((error, data, message) => {
+    if (!error && !data) {
+        console.info(message);
+    }
+  })("Log", `Shield Points: ${pointsValue}`);
+
+  const jsonData = JSON.stringify({ shieldPoints: pointsValue });
+  exports.send(jsonData);
 }
+
+async function requestGetWithOptions(options) {
+  const res = await request(options);
+  return res.body;
+}
+
+exports.callbackLogger = event => {
+  console.log(arguments);
+};
+
+
--- main.js
+++ main.js
@@ -1,54 +1,76 @@
 const getUsersScore = async (username) => {
-  console.log("Getting score for " + username)
-  const response = null//await fetch(`/api/fetchScores?username=${username}`);
-  const data = { score: 1300 }//await response.json();
-  console.log("data for " + username + " is " + data.score + " points");
-  return data.score;
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const targetUrl = `https://www.jetpunk.com/users/${username}`;
+  const response = await fetch(proxyUrl + targetUrl);
+  const data = await response.text();
+
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(data, 'text/html');
+  const scoreElement = doc.querySelector('.shield-total .level-points');
+  
+  if (scoreElement) {
+    errorAlert(false);
+    const score = parseInt(scoreElement.textContent.replace(/,/g, ''), 10);
+    return score;
+  }else{
+    errorAlert(true);
+    throw new Error('CORS Proxy Not Enabled');
+  }
 };
   
-  const createBar = (username, score, maxScore, color) => {
-    console.log(`Creating bar for ${username} with score ${score}`);
-    const barContainer = document.createElement('div');
-    barContainer.classList.add('bar-container');
+const createBar = (username, score, maxScore, color) => {
+  const barContainer = document.createElement('div');
+  barContainer.classList.add('bar-container');
+
+  const label = document.createElement('div');
+  label.classList.add('bar-label');
+  label.textContent = username;
+  barContainer.appendChild(label);
+
+  const bar = document.createElement('div');
+  bar.classList.add('bar');
+  const chartHeight = 300;
+  const barHeight = (score / maxScore) * chartHeight;
+  bar.style.height = `${barHeight}px`;
+  bar.style.backgroundColor = color;
+  barContainer.appendChild(bar);
+
+  const scoreLabel = document.createElement('div');
+  scoreLabel.classList.add('score-label');
+  scoreLabel.textContent = score;
+  bar.appendChild(scoreLabel);
+
+  return barContainer;
+};
   
-    const label = document.createElement('div');
-    label.classList.add('bar-label');
-    label.textContent = username;
-    barContainer.appendChild(label);
+const updateScores = async () => {
+  try {
+    const users = ['Tuperwak', 'Voture'];
+    const colors = ['#4bc0c0', '#f56565'];
+    const scores = await Promise.all(users.map(getUsersScore));
   
-    const bar = document.createElement('div');
-    bar.classList.add('bar');
-    const chartHeight = 300;
-    const barHeight = (score / maxScore) * chartHeight;
-    bar.style.height = `${barHeight}px`;
-    bar.style.backgroundColor = color;
-    barContainer.appendChild(bar);
+    const chart = document.getElementById('chart');
+    chart.innerHTML = '';
+    const maxScore = Math.max(...scores);
   
-    const scoreLabel = document.createElement('div');
-    scoreLabel.classList.add('score-label');
-    scoreLabel.textContent = score;
-    bar.appendChild(scoreLabel);
-  
-    return barContainer;
-  };
-  
-  const updateScores = async () => {
-    try {
-      const users = ['Tuperwak', 'Voture'];
-      const colors = ['#4bc0c0', '#f56565'];
-      const scores = await Promise.all(users.map(getUsersScore));
-    
-      const chart = document.getElementById('chart');
-      chart.innerHTML = '';
-      const maxScore = Math.max(...scores);
-    
-      users.forEach((username, index) => {
-        const bar = createBar(username, scores[index], maxScore, colors[index]);
-        chart.appendChild(bar);
-      });
-    } catch (error) {
-      console.error('Error updating scores:', error);
-    }
-  };
-  //setInterval(updateScores, 3000);
-  updateScores();
+    users.forEach((username, index) => {
+      const bar = createBar(username, scores[index], maxScore, colors[index]);
+      chart.appendChild(bar);
+    });
+  } catch (error) {
+    console.error('Error updating scores:', error);
+  }
+};
+
+const errorAlert = (bool) => {
+  const alert = document.getElementById('alert');
+  alert.style.display = bool ? 'inline' : 'none';
+}
+
+const handleError =() => {
+  window.open("https://cors-anywhere.herokuapp.com/corsdemo", '_blank').focus();
+  errorAlert(false);
+}
+
+updateScores();
+setInterval(updateScores, 2000);
